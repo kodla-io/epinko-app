8047238e1b25423ab0294ec912863dbaa2f6f682
kodla-io <56536150+MehmetCakirkaya@users.noreply.github.com>
2025-07-05 00:17:44 +0300

    error fixed

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,10 @@
-import  ProfileTabs from "../../../componants/profile/profile-tabs";
+import { Suspense } from "react";
+import ProfileTabs from "../../../componants/profile/profile-tabs";
 
 export default function GameDetail() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <ProfileTabs />
-    </>
+    </Suspense>
   );
 }
